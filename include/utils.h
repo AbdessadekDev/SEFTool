0cb2330f88af920c811f92ef3878a94d115770ce
abdessadekdev <sbaaiabdessadek@gmail.com>
2024-11-13 19:41:26 +0100

    Add documentation and file handling functions to utils.h: isFileExists, countLines, writeLines, and removeEncExtension

--- include/utils.h
+++ include/utils.h
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #ifndef UTILS_H
 #define UTILS_H
 
@@ -27,5 +28,43 @@ void byteToHex(unsigned char* bytes, int bytesLen, char *hex);
  * @param length  Pointer to store the length of the resulting byte array.
  */
 void hexToBytes(const char *hex, unsigned char **bytes, size_t *length);
+
+/**
+ * @brief Checks if a file exists at the specified path.
+ *
+ * @param fileName The name of the file to check.
+ * @return true if the file exists, false if it does not exist or if an error occurs.
+ */
+bool isFileExists(char *fileName);
+
+/**
+ * @brief Counts the number of lines in a file.
+ *
+ * @param filename The name of the file to count lines in.
+ * @return The number of lines in the file, or -1 if the file cannot be opened.
+ */
+int countLines(const char *filename);
+
+/**
+ * @brief Writes an array of encrypted lines to a binary file.
+ *
+ * @param fileName The name of the file to write to.
+ * @param lines A 2D array of encrypted lines to be written to the file.
+ * @param lineLengths An array containing the length of each line in `lines`.
+ * @param linesSize The number of lines to write.
+ * @return 0 if writing is successful, or an error code (-33 if the file cannot be opened, 
+ *         -34 if there is a write error).
+ */
+int writeLines(const char *fileName, unsigned char **lines, int *lineLengths, int linesSize);
+
+
+/**
+ * @brief Removes the ".enc" extension from a filename.
+ *
+ * @param fileName The original file name with the ".enc" extension.
+ * @param outputFile The output buffer to store the filename without the ".enc" extension.
+ */
+void removeEncExtension(const char *fileName, char *outputFile);
+
 
 #endif /* UTILS_H */
