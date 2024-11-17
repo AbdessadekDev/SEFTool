366ad94b28080445d0c4c6d02c235907de5ec679
abdessadekdev <sbaaiabdessadek@gmail.com>
2024-11-17 17:50:35 +0100

    Refactor and expand authentication and encryption functionalities
    - Added specific enums for error handling in user loading and saving processes.
    - Introduced new functions for user authentication and session management (, , , ).
    - Expanded encryption and decryption error handling with detailed error codes.
    - Improved key and IV setup process with a dedicated  function.
    - - Refined error codes for encryption and file operations to provide clearer failure information.

--- include/sfetool.h
+++ include/sfetool.h
@@ -5,6 +5,7 @@
 
 
 #define USERS_FILE "data/users.dat"
+#define LOGIN_FILE "data/login.enc"
 #define USER_INFO_MAX 300
 #define SFETOOL_KEY_LEN 16 // AES-128 requires a 16-byte key
 
@@ -40,6 +41,34 @@ typedef enum {
     DECRYPTFILE_CONTEXT_ERROR = -201,        // Decryption context initialization error
     DECRYPTFILE_DECRYPTION_ERROR = -202      // Error during the decryption process
 } DecryptFileCode;
+
+typedef enum {
+    ENCRYPT_SUCCESS,
+    ENCRYPT_INIT_CXT_ERR,
+    ENCRYPT_INIT_ENCRYPTION,
+    ENCRYPT_UPDATE_ENCRYPTION,
+    ENCRYPT_FINAL_ENCRYPTION,
+} EncryptCode;
+
+typedef enum {
+    SUCCESS = 0,
+    ERR_FILE_OPEN_FAILED = -100,
+    ERR_MEMORY_ALLOCATION_FAILED = -102,
+    ERR_SFETOOL_KEY_OR_IV_MISSING = -12,
+    ERR_KEY_IV_ALLOCATION_FAILED = -103,
+    ERR_INVALID_KEY_IV_LENGTH = -104,
+    ERR_ENCRYPTION_FAILED = -13,
+    ERR_TOKEN_CREATION_FAILED = -101,
+    ERR_FILE_READ_FAILED = -105
+} StatusCode;
+
+typedef enum {
+    SUCCESS = 0,
+    FILE_OPEN_ERROR,
+    FILE_WRITE_ERROR,
+    FILE_READ_ERROR,
+    FILENAME_NOT_FOUND
+} StatusCodeCSV;
 
 /**
  * Loads user data from a file and populates an array of User structs.
@@ -124,6 +153,58 @@ void setupKeys();
  *          - 0 if the email isn't exists.
  *          - a nigative number if an error occur during load users.
  */
-int isDuplicatedEmail(const char *email);
+int isEmailExists(const char *email);
+/**
+ * Fetches the user information based on the provided email address.
+ * 
+ * @param email A string representing the user's email address to fetch the user information.
+ * 
+ * @return A pointer to a `User` structure containing the user's details, or `NULL` if no user is found.
+ * 
+ */
+User *getUserInfo(const char *email);
+
+/**
+ * Creates a session for the user with the given email.
+ * 
+ * @param email The email address of the user to create a session for.
+ * 
+ * @return `0` on success (session created), a nigative number where it's fails.
+ */
+int createSession(const char *email);
+
+/**
+ * Checks if there is an active session.
+ * 
+ * @return `0` on success, a nigative number where it's fails.
+ */
+int checkSession();
+
+/**
+ * Encrypts the input data using the specified key and initialization vector (IV).
+ * 
+ * @param input (unsigned char *): A pointer to the input data to be encrypted.
+ * @param inputLength (int): The length of the input data in bytes.
+ * @param output (unsigned char *): A pointer to the memory where the encrypted data will be stored.
+ * @param key (unsigned char *): The encryption key.
+ * @param iv (unsigned char *): The initialization vector used for encryption.
+ * 
+ * @return 0 on successful encryption, nigative code number on fails.
+ */
+int encrypt(unsigned char *input, int inputLength, unsigned char *output, unsigned char *key, unsigned char *iv);
+int decrypt(unsigned char *input, int inputLength, unsigned char *output, unsigned char *key, unsigned char *iv);
+
+int computeFileHash(const char *filename, unsigned char *hash_out, unsigned int *hash_len);
+void printHash(unsigned char *hash, unsigned int hash_length);
+
+/**
+ * append a record to the CSV file
+ */
+int appendToCsv(const char *csvFileName, const char *fileName, const char *hash);
+
+/**
+ * Read the hash for a given filename from the CSV file
+ */
+int readFromCsv(const char *csvFileName, const char *fileName, char *hashBuffer, size_t bufferSize);
 
 #endif
